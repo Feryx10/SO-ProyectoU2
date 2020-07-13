c43f6db65e0938faa7c83badc4c6e70596af78c2
Luciano <lnicolas15@alumnos.utalca>
2020-07-12 23:20:07 -0400

    Generador de labs y docs
    aleatorios

--- src/proyectosou2/TestThread.java
+++ src/proyectosou2/TestThread.java
@@ -22,7 +22,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import static java.lang.Thread.sleep;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -60,7 +62,7 @@ class procesarCaso extends Thread  {
    public void run() {
       synchronized(PD) {
          PD.printCount(nombreRegion, fuente);
-         new Fichero().escribir(nombreRegion, new Caso());
+    //     new Fichero().escribir(nombreRegion, new Caso());
       }
     //  System.out.println("Thread " +  nombreRegion+", "+fuente + " exiting.");
       
@@ -79,8 +81,8 @@ public class TestThread {
 
     static Region region1 = new Region("Maule");
     static Region region2 = new Region("Bio-Bio");
-    
-   public static void main(String args[]) {
+
+   public static void main(String args[]) throws InterruptedException {
       
         
        
@@ -92,7 +94,7 @@ public class TestThread {
         while(program) {
             System.out.println("");
             System.out.println("");
-            System.out.println("[1] Ingrese como administrador");
+            System.out.println("[1] Generar N doctores y laboratorios aleatorios por región");
             System.out.println("[2] Generar N casos aleatorios");
             System.out.println("[3] Iniciar Simulación");
             System.out.println("[4] Funciones del servicio de salud");
@@ -109,39 +111,20 @@ public class TestThread {
             }
             
             if (entrada==1) {
-                int ent2=0;
-                System.out.println("");
-                System.out.println("[1] Generar N doctores y laboratorios aleatorios");
-                System.out.print("Ingrese un número: ");
-                ent2=sc.nextInt();
+   
+                int cant_doctores;
+                int cant_lab;
+
+                System.out.print("Ingrese cantidad de doctores: ");
+                cant_doctores=sc.nextInt();
+                System.out.print("Ingrese cantidad de laboratorios: ");
+                cant_lab = sc.nextInt();
                 
-                if (ent2==1) {
-                    System.out.println("");
-                    System.out.println("Elija región");
-                    System.out.println("[1] Región A");
-                    System.out.println("[2] Región B");
-                    System.out.print("Ingrese un número: ");
-                    region=sc.nextInt();
-                    
-                    if (region>2 || region <1) {
-                        System.out.println("");
-                        System.out.println("Entrada inválida");
-                        System.out.println("");
-                    }
-                    else {
-                        int cant_doctores=0;
-                        int cant_lab=0;
-                        
-                        System.out.print("Ingrese cantidad de doctores: ");
-                        cant_doctores=sc.nextInt();
-                        System.out.print("Ingrese cantidad de laboratorios: ");
-                        cant_lab = sc.nextInt();
-                    }
-                }
-                else {
-                    System.out.println("Entrada inválida");
-                }                                      
+                crearFuentes(cant_doctores, cant_lab);
+                
+                                                    
             }
+        
             
             if (entrada==2) {
                 System.out.println("");
@@ -171,6 +154,7 @@ public class TestThread {
             if (entrada==3) {
                 
                 int dias=0;
+                iniciarSimulacion();
                 System.out.print("Ingrese cantidad de días: ");
                 dias=sc.nextInt();
             }
@@ -198,30 +182,146 @@ public class TestThread {
       
    }
    
-   public static void iniciarSimulacion()
-   {       
+   public static void generarNombres(int cantDocs, int cantLabs)
+   {
+       //http://ensaimeitor.apsl.net/personas
+       
+       String nombres = "Salustiano Zorrilla Morelos \n" +
+                        "Octavio Sariego Guinda \n" +
+                        "Oliver Hesles Ledrado \n" +
+                        "Gumersindo Medeiros Izcaray \n" +
+                        "Luciana Bravo Errasti \n" +
+                        "Zineb Maoño Cos \n" +
+                        "Ekaterina Puebla Limofer \n" +
+                        "Iovani Lanis Cediel \n" +
+                        "Paola Pison Megid \n" +
+                        "Nara Boliana Gayferos \n" +
+                        "Tamara Francisco Soterraña \n" +
+                        "Leonardo Yurre Ubidea \n" +
+                        "Josué Guardo Robredo \n" +
+                        "Verónica Cuba Almandoz \n" +
+                        "Sara Cubero Lebrija \n" +
+                        "Kimberly Tamaron Oribe \n" +
+                        "Rodame Basante Santiyan \n" +
+                        "Magnolia Sarricolea Cotorro \n" +
+                        "Luana Arrieta Olivarez \n" +
+                        "Celia Valdevielso Albarellos";
+       
+        String laboratorios =   "Hamaren\n" +
+                                "Vors\n" +
+                                "Creative Envirole\n" +
+                                "Nasketz\n" +
+                                "Soapsounds\n" +
+                                "Noctorspot\n" +
+                                "Fastbook\n" +
+                                "Future Life Body\n" +
+                                "Mass Yarns\n" +
+                                "Websporthh\n" +
+                                "Leaper\n" +
+                                "Shopmingde\n" +
+                                "Hings\n" +
+                                "Mmsrd\n" +
+                                "BoxPlate\n"+
+                                "FGI EZ Networks\n" +
+                                "Aks Corral\n" +
+                                "AGP Insights\n" +
+                                "Frame Canopy\n" +
+                                "Itresphapi\n" +
+                                "Ishaw\n" +
+                                "Ultimate Trolley\n" +
+                                "Jmlpa\n" +
+                                "Bloom.fm\n" +
+                                "Wolmbm\n" +
+                                "ShampooBaker\n" +
+                                "Ubega\n" +
+                                "Writiner";
+       
+        String[] arrDocs = nombres.split("\n"); 
+  
+        for (int i=0; i<cantDocs; i++) 
+        {
+            Fuente doctor = new Fuente(arrDocs[i]);
+            region1.agregarDoctor(doctor);
+        }
+        
+        for (int i=0; i<cantDocs; i++) 
+        {
+            Fuente doctor = new Fuente(arrDocs[cantDocs-1-i]);
+            region2.agregarDoctor(doctor);
+        }
+        
+        String[] arrLabs = laboratorios.split("\n"); 
+  
+        for (int i=0; i<cantLabs; i++)
+        {
+            Fuente doctor = new Fuente("Laboratorio "+arrLabs[i]);
+            region1.agregarLaboratorio(doctor);
+        }
+        
+        for (int i=0; i<cantLabs; i++) 
+        {
+            Fuente doctor = new Fuente("Laboratorio "+arrLabs[cantLabs-1-i]);
+            region2.agregarLaboratorio(doctor);
+        }
+        
+   }
+   
+   
+   
+   
+   public static void iniciarSimulacion() throws InterruptedException
+   {    
+       
+ 
         PrintDemo PD1 = new PrintDemo();
         PrintDemo PD2 = new PrintDemo();
-        procesarCaso regionUnoLaboratirios = new procesarCaso( region1, PD1, "Lab" );
-        procesarCaso regionUnoDoctores = new procesarCaso( region1, PD2, "Doc" );
-        procesarCaso regionDosLaboratorios = new procesarCaso( region2, PD1, "Lab");
-        procesarCaso regionsDosDoctorees = new procesarCaso( region2, PD2, "Doc" );
-        regionUnoLaboratirios.start();
-        regionUnoDoctores.start();
-        regionDosLaboratorios.start();
-        regionsDosDoctorees.start();
+        
+        
+        
 
       // wait for threads to end
-        try {
-            regionUnoLaboratirios.join();
-            regionUnoDoctores.join();
-            regionDosLaboratorios.join();
-            regionsDosDoctorees.join();
-        } catch ( Exception e) {
-            System.out.println("Interrupted");
+        for (int i = 1; i < 10; i++) {
+            
+            System.out.println("Dia: "+i);
+            procesarCaso regionUnoLaboratirios = new procesarCaso( region1, PD1, "Lab" );
+            procesarCaso regionUnoDoctores = new procesarCaso( region1, PD2, "Doc" );
+            procesarCaso regionDosLaboratorios = new procesarCaso( region2, PD1, "Lab");
+            procesarCaso regionsDosDoctorees = new procesarCaso( region2, PD2, "Doc" );
+            regionUnoLaboratirios.start();
+            regionUnoDoctores.start();
+            regionDosLaboratorios.start();
+            regionsDosDoctorees.start();
+            regionUnoLaboratirios.start();
+            regionUnoDoctores.start();
+            regionDosLaboratorios.start();
+            regionsDosDoctorees.start();
+            try {
+                regionUnoLaboratirios.join();
+                regionUnoDoctores.join();
+                regionDosLaboratorios.join();
+                regionsDosDoctorees.join();
+            } 
+            catch ( Exception e) 
+            {
+                System.out.println("Interrupted");
+            }
+            TimeUnit.SECONDS.sleep(1);
+           
         }
-   }   
+        
+        
+    } 
+
+    private static void crearFuentes(int cant_doctores, int cant_lab) {
+        for (int i = 0; i < cant_doctores; i++)
+        {
+      //      region1.agregarDoctor();
+        }
+        generarNombres(cant_doctores, cant_lab);
+    }
 }
+
+
 
 class Fichero implements FuncionFichero {
     
@@ -260,5 +360,5 @@ class Fichero implements FuncionFichero {
            System.out.println(ex.getMessage());
        }
        return aux;
-    }    
+    }
 }
